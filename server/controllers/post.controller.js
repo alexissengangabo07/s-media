218f4acbe0e87e6b9a47d9b4a666c16723808c77
alexissengangabo07 <alexxngabo@gmail.com>
2022-08-14 18:47:21 +0200

    feat: create post model

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -1,3 +1,14 @@
+import postModel from "../models/post.model.js";
+
 export const getPosts = (req, res) => {
     res.send('This work !');
+}
+
+export const createPost = async (req, res) => {
+    try {
+        const posts = await postModel.find();
+        res.status(200).json(posts)
+    } catch (error) {
+        res.status(400).json({message: error});
+    }
 }

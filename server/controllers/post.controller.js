230c9f52e2effa0bf4313e5d6bec50c40af07149
alexissengangabo07 <alexxngabo@gmail.com>
2022-08-15 16:32:49 +0200

    feat: mount api

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -1,14 +1,20 @@
 import postModel from "../models/post.model.js";
 
-export const getPosts = (req, res) => {
-    res.send('This work !');
+export const getPosts = async (req, res) => {
+    try {
+        const posts = await postModel.find();
+        res.status(200).json(posts);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 }
 
 export const createPost = async (req, res) => {
+    const newPost = new postModel(req.body);
     try {
-        const posts = await postModel.find();
-        res.status(200).json(posts)
+        await newPost.save();
+        res.status(201).json({message: 'post saved'});
     } catch (error) {
-        res.status(400).json({message: error});
+        res.status(400).json({ message: error.message });
     }
 }

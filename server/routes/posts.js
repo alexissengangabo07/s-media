a1c9cc9a65e9ef83802c0aa71e59063577468c2e
alexissengangabo07 <alexxngabo@gmail.com>
2022-09-16 09:05:15 +0200

    reinitial project

--- server/routes/posts.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getPosts, createPost } from '../controllers/post.controller.js';
-
-const router = express.Router();
-
-router.get('/', getPosts);
-router.post('/', createPost);
-
-export default router;

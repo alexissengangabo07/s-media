e224a66b8f5393187fbd459cce7774d895d983e7
alexissengangabo07 <alexxngabo@gmail.com>
2022-09-30 16:01:09 +0200

    refact body parser with express

--- server/index.js
+++ server/index.js
@@ -1,14 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
-import bodyParser from "body-parser";
+import cors from 'cors';
 
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1/users_ejs_crud');
+mongoose.connect('mongodb://127.0.0.1/mern_mastery');
 const db = mongoose.connection;
 db.once('open', () => console.log('DATABASE CONNECTED SUCCESSFULY'));
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: '30mb', extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.listen(5000, () => {
     console.log('Okay, Server is running at 5000');

cff264ac1140cccd5185bd6565eeddbedd13033a
alexissengangabo07 <alexxngabo@gmail.com>
2022-10-30 07:38:16 +0200

    style review

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Post from './post/Post';
 import { useSelector } from 'react-redux';
+import './style.css';
 
 const Posts = () => {
   const posts = useSelector((state) => state.postReducer);
